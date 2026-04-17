5a6d4df2317c6fa33938cb72133889e90edd2788
Ismael Britzke <isma_steffens@hotmail.com>
2026-04-16 22:42:03 -0300

    ajuste app

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
+const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://fitpro-frontend-6t6x4jnrc-ibritzkes-projects.vercel.app",
@@ -17,10 +17,14 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-}));
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions)); // <- ESSENCIAL
+
 app.use(express.json());
 
-// Servir imagens publicamente
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use(routes);
